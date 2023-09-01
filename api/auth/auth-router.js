5ea74ae4959852c12e99c19b193e1580c9da0b7f
Timothy White <bigtball321@yahoo.com>
2023-09-01 15:45:45 -0400

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,6 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
-
+const router = require('express').Router()
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -24,7 +24,9 @@
     "message": "Password must be longer than 3 chars"
   }
  */
-
+router.post('/register', (req, res, next) => {
+  res.json('register')
+})
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -41,7 +43,9 @@
     "message": "Invalid credentials"
   }
  */
-
+  router.post('/register', (req, res, next) => {
+    res.json('register')
+  })
 
 /**
   3 [GET] /api/auth/logout
@@ -57,7 +61,9 @@
   {
     "message": "no session"
   }
- */
-
+ */router.get('/logout', (req, res, next) => {
+  res.json('logout')
+})
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,6 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
-
-
+const router = require('express').Router()
+const { restricted,} = require('../auth/auth-middleware')
 /**
   [GET] /api/users
 
@@ -23,6 +23,11 @@
     "message": "You shall not pass!"
   }
  */
+  router.get('/', restricted, (req,res, next) => {
+    res.json('users')
+  })
 
+  
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router 
