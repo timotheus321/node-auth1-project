89f1ef34b65f60ad4646f92a2d1ba1e75d627fba
Timothy White <bigtball321@yahoo.com>
2023-09-01 17:03:00 -0400

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -25,7 +25,7 @@ try {
   if (!users.length) {
     next()
   } else {
-    next({ "message": "Username taken"})
+    next({ message: "Username taken", status: 422})
   }
 } catch (err) {
   next(err)
@@ -40,8 +40,17 @@ try {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists(req, res, next) {
-next()
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username })
+    if (!users.length) {
+      next()
+    } else {
+      next({ message: "Invalid credentials", status: 401})
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
